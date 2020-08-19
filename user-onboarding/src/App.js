70b19a2be8e00f8a10d0308db26bd6f0c5645cd8
Sara Cearc <scearc930@gmail.com>
2020-08-19 15:08:47 -0400

    imported form, axios, yup to app, and added inital information to app

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,23 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import Form from './Form';
 import './App.css';
+import axios from 'axios';
+import * yup from 'yup';
+
+const initialFormValues = {
+  name: '',
+  email: '',
+  password: '',
+}
+
+const initialFormErrors ={
+  name: '',
+  email: '',
+  password: '',
+}
+
+const initialUsers = []
+const initalDisabled = true
 
 function App() {
+  const [users, setUsers] = useState(initialUsers)
+  const [formValues, setFormValues] = useState(initialFormValues) 
+  const [formErrors, setFormErrors] = useState(initialFormErrors) 
+  const [disabled, setDisabled] = useState(initialDisabled) 
+
+const getUsers = () => {
+  axios.get('https://reqres.in/api/users')
+  .then(res => {
+    setUsers(res.data)
+  })
+  .catch(err => {
+    debugger
+  })
+}
+
+const postNewUser = newUser => {
+  axios.post('https://reqres.in/api/users', newUser)
+  .then(res => {
+    setUsers(users.concat(res.data))
+  })
+  .catch(err => {
+    debugger
+  })
+  .finally(() => {
+    setFormValues(initialFormValues)
+  })
+}
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
